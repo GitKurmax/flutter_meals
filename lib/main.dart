9004bc89d5af6422ac3643d8d141c57b46926b9f
alex <oleksandr.kurmoiarov@smartexe.com>
2022-06-07 17:57:43 +0300

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:meals/category_item.dart';
+
+import './categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
